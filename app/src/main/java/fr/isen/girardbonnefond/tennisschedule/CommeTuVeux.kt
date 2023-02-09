4b85ca616081cf1d8c1be898fa8ce8718a2643e5
kmillegirard <camille.girard@isen.yncrea.fr>
2023-02-09 19:37:49 +0100

    log en cours

---
+++ app/src/main/java/fr/isen/girardbonnefond/tennisschedule/CommeTuVeux.kt
@@ -0,0 +1,11 @@
+package fr.isen.girardbonnefond.tennisschedule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommeTuVeux : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comme_tu_veux)
+    }
+}
--- app/src/main/java/fr/isen/girardbonnefond/tennisschedule/MainActivity.kt
+++ app/src/main/java/fr/isen/girardbonnefond/tennisschedule/MainActivity.kt
@@ -1,16 +1,116 @@
 package fr.isen.girardbonnefond.tennisschedule
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import fr.isen.girardbonnefond.tennisschedule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var auth: FirebaseAuth
+
+    lateinit var email:String
+    lateinit var password:String
 
     companion object{
         var url = "https://tennisschedule-ecba9-default-rtdb.europe-west1.firebasedatabase.app/"
+        private const val TAG = "EmailPassword"
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*email = binding.email.text.toString()
+        password = binding.password.text.toString()*/
+        auth = Firebase.auth
+        onStart()
+        binding.buttonConnexion.setOnClickListener{
+            email = binding.email.text.toString()
+            password = binding.password.text.toString()
+            signIn(email,password)
+        }
     }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload()
+        }
+    }
+    // [END on_start_check_user]
+
+    private fun createAccount(email: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun sendEmailVerification() {
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // Email Verification sent
+            }
+        // [END send_email_verification]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        val intent= Intent(this,CommeTuVeux::class.java)
+        startActivity(intent)
+
+        ///If user diff null afficher autre page
+
+    }
+
+    private fun reload() {
+
+    }
+
+
 }
